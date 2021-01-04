a4c186b6a5c533e5cb383b0c73635a4fc68dcbb5
JC <james.calingo@gmail.com>
2021-01-04 11:25:00 -0500

    fixed map centering

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,26 +1,26 @@
-var map;
-var service;
-var infowindow;
+let map;
+let service;
+let infowindow;
 
 function initMap() {
-  var defaultLocation = new google.maps.LatLng(40.758611, -73.976389);
+  let defaultLocation = new google.maps.LatLng(40.758611, -73.976389);
 
   infowindow = new google.maps.InfoWindow();
 
   map = new google.maps.Map(
       document.getElementById('map'), {center: defaultLocation, zoom: 15});
 
-  var request = {
+  let request = {
     query: 'lutheran',
     fields: ['name', 'geometry', 'type'],
   };
 
-  var service = new google.maps.places.PlacesService(map);
+  let service = new google.maps.places.PlacesService(map);
 
   service.findPlaceFromQuery(request, function(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
       console.log(results)
-      for (var i = 0; i < results.length; i++) {
+      for (let i = 0; i < results.length; i++) {
         createMarker(results[i]);
       }
       map.setCenter(results[0].geometry.location);
