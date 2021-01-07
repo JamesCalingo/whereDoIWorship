5f0c3a9debeb616f65a07470d57bff5e9ac96b8f
JC <james.calingo@gmail.com>
2021-01-07 11:58:16 -0500

    List function added

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,52 +1,61 @@
-const detailDiv = document.querySelector("#details")
-const select = document.querySelector("#denomination")
-const button = document.querySelector("#search")
-const locationInput = document.querySelector("#locationInput")
+const select = document.querySelector("#denomination");
+const button = document.querySelector("#search");
+const locationInput = document.querySelector("#locationInput");
+const detailDiv = document.querySelector("#details");
+const list = document.querySelector("ol");
 
 let map;
 let service;
 let infowindow;
 
 function initMap() {
-  
   let defaultLocation = new google.maps.LatLng(40.758611, -73.976389);
 
   infowindow = new google.maps.InfoWindow();
 
-  map = new google.maps.Map(
-      document.getElementById('map'), {center: defaultLocation, zoom: 15});
-
-  
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: defaultLocation,
+    zoom: 15,
+  });
 }
 
-function search () {
-  let searchValue = select.value
-  console.log(searchValue)
-  let locationValue = locationInput.value
+function useAutoComplete () {
+  const autocomplete = new google.maps.places.Autocomplete(locationInput);
+}
+
+function search() {
+  let searchValue = select.value;
+  let locationValue = locationInput.value || "New York City";
+
   let request = {
     location: new google.maps.LatLng(40.758611, -73.976389),
     type: searchValue,
-  
-    radius: 10000
+    radius: 10000,
   };
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: request.location,
+    zoom: 15,
+  });
 
   let service = new google.maps.places.PlacesService(map);
 
-  service.nearbySearch(request, function(results, status) {
-    if(!searchValue) return
+  service.nearbySearch(request, function (results, status) {
+    if (!searchValue || !locationValue) return;
     else if (status === google.maps.places.PlacesServiceStatus.OK) {
-      console.log(results)
+      console.log(results);
       for (let i = 0; i < results.length; i++) {
         createMarker(results[i]);
         detailDiv.innerHTML = `
-        I found this:<br>
-        ${results[i].name}<br>
-        ${results[i].vicinity}
-        `
+        Here's what I found, sorted by distance. Hope this helps!
+        `;
+        let li = document.createElement("li");
+        li.innerHTML = `<b>${results[i].name}</b><br>
+       ${results[i].vicinity}
+       `;
+        list.appendChild(li);
       }
-      map.setCenter(results[0].geometry.location);
-
-   
+      // map.setCenter(results[0].geometry.location);
     }
   });
 }
@@ -62,4 +71,5 @@ function createMarker(place) {
   });
 }
 
-button.addEventListener("click", search)
+button.addEventListener("click", search);
+locationInput.addEventListener("change", useAutoComplete)
