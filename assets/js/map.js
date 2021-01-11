7d461a6774d3859d61ee06a193a9b0f85f19b1cb
JC <james.calingo@gmail.com>
2021-01-11 16:59:37 -0500

    geolocation working

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,3 @@
-
 const select = document.querySelector("#denomination");
 const button = document.querySelector("#search");
 const locationInput = document.querySelector("#locationInput");
@@ -8,6 +7,7 @@ const list = document.querySelector("ol");
 let map;
 let service;
 let infowindow;
+
 
 function initMap() {
   let defaultLocation = new google.maps.LatLng(40.758611, -73.976389);
@@ -20,29 +20,40 @@ function initMap() {
   });
 }
 
+
+
 function useAutoComplete () {
   const autocomplete = new google.maps.places.Autocomplete(locationInput);
 }
 
-function search(lat, lon) {
+function search() {
   list.innerHTML = ""
   let searchValue = select.value;
   let locationValue = locationInput.value || "New York City";
 
-  let request = {
-    location: new google.maps.LatLng(40.758611, -73.976389),
-    type: searchValue,
-    radius: 10000,
-  };
+  let request
 
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: request.location,
-    zoom: 15,
-  });
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let lat = pos.coords.latitude
+      let lon = pos.coords.longitude
+      request = {
+        location: new google.maps.LatLng(lat, lon),
+        type: searchValue,
+        radius: 10000
+      }
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: request.location,
+        zoom: 15,
+      });
+  
+  
 
   let service = new google.maps.places.PlacesService(map);
 
+    console.log(request)
   service.nearbySearch(request, function (results, status) {
+    console.log(status)
     if (!searchValue || !locationValue) return;
    
     else if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -58,10 +69,11 @@ function search(lat, lon) {
        `;
         list.appendChild(li);
       }
-      // map.setCenter(results[0].geometry.location);
     }
   });
+})
 }
+
 
 function createMarker(place) {
   const marker = new google.maps.Marker({
