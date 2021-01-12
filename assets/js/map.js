cce299d4c1695865420fd2f22ea471da2f974af8
JC <james.calingo@gmail.com>
2021-01-12 18:44:21 -0500

    updating a few search things

--- assets/js/map.js
+++ assets/js/map.js
@@ -20,60 +20,55 @@ function initMap() {
   });
 }
 
-
-
-function useAutoComplete () {
+function useAutoComplete() {
   const autocomplete = new google.maps.places.Autocomplete(locationInput);
 }
 
 function search() {
-  list.innerHTML = ""
+  list.innerHTML = "";
+  console.log(select)
   let searchValue = select.value;
   // let locationValue = locationInput.value || "New York City";
 
-  let request
+  let request;
 
+  navigator.geolocation.getCurrentPosition((pos) => {
+    let lat = pos.coords.latitude;
+    let lon = pos.coords.longitude;
+    request = {
+      location: new google.maps.LatLng(lat, lon),
+      keyword: searchValue,
+      rankBy: google.maps.places.RankBy.DISTANCE,
+    };
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: request.location,
+      zoom: 15,
+    });
 
-    navigator.geolocation.getCurrentPosition((pos) => {
-      let lat = pos.coords.latitude
-      let lon = pos.coords.longitude
-      request = {
-        location: new google.maps.LatLng(lat, lon),
-        keyword: searchValue,
-        rankBy: google.maps.places.RankBy.DISTANCE,
-      }
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: request.location,
-        zoom: 15,
-      });
-  
-  
+    let service = new google.maps.places.PlacesService(map);
 
-  let service = new google.maps.places.PlacesService(map);
-
-    console.log(request)
-  service.nearbySearch(request, function (results, status) {
-    console.log(status)
-    if (!searchValue) return;
-   else if (status === google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
-     alert("Unfortunately, I was unable to find any results. Sorry.")
-   }
-    else if (status === google.maps.places.PlacesServiceStatus.OK) {
-      console.log(results);
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-        detailDiv.innerHTML = `
+    console.log(request);
+    service.nearbySearch(request, function (results, status) {
+      console.log(status);
+      if (!searchValue) return;
+      else if (status === google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
+        alert("Unfortunately, I was unable to find any results. Sorry.");
+      } else if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
+        for (let i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+          detailDiv.innerHTML = `
         Here are some places that I found near you. Hope this helps!
         `;
-        let li = document.createElement("li");
-        li.innerHTML = `<b>${results[i].name}</b><br>
+          let li = document.createElement("li");
+          li.innerHTML = `<b>${results[i].name}</b><br>
        ${results[i].vicinity}
        `;
-        list.appendChild(li);
+          list.appendChild(li);
+        }
       }
-    }
+    });
   });
-})
 }
 
 function createMarker(place) {
