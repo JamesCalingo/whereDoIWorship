dc7d099a671eeccbaf4ea5b00db61cd4db7072e9
JC <james.calingo@gmail.com>
2021-04-19 14:47:57 -0400

    updates to toggle function

--- assets/js/toggleDark.js
+++ assets/js/toggleDark.js
@@ -4,6 +4,8 @@ const toggleBtn = document.querySelector("#toggle");
 function toggleDark() {
   const body = document.querySelector("body");
   body.classList.toggle("dark");
+  console.log(body.classList.contains("dark"))
 }
+
 
 toggleBtn.addEventListener("click", toggleDark);
